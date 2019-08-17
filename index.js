a57662bf4bb22721c99401eddb0cd8fe9ecb9d7b
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-17 20:27:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ function BoardMember(name, homeState, training){
   this.veto = function(){
     return "No, I must disagree"
   }
+
   this.approve = function(){
     return "You can do that!"
+  }
+
+  this.doCharity = function(){
+    return "I like to help people"
   }
 }
